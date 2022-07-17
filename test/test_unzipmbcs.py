f3a521379e0ac10bf70e9db279e86053bf590f58
Joo-Won Jung <sanori@gmail.com>
2022-07-17 20:14:12 +0900

    chore: add encrypted zip testcase with full-width charater password

--- test/test_unzipmbcs.py
+++ test/test_unzipmbcs.py
@@ -17,11 +17,44 @@ class TestFromZip(unittest.TestCase):
         self.assertEqual(list(map(lambda x: x[0], result)), self.expected)
 
     def testExtractZip(self):
-        if (sys.getfilesystemencoding() != 'UTF-8') and (not os.environ.get('PYTHONIOENCODING')):
+        if (sys.getfilesystemencoding().lower() != 'utf-8') and (not os.environ.get('PYTHONIOENCODING')):
             print('Warning: non-UTF8 filesystem.',
                   'set PYTHONIOENCODING as your filesystem encoding!')
             return
         unzipmbcs.extractZip(self.filename, self.encoding)
+        map(lambda x: self.assertTrue(os.path.exists(x), x + ' not exist'),
+            self.expected)
+
+        # clean-up
+        files = list(self.expected)   # clone the list
+        files.reverse()
+        for f in files:
+            if (os.path.isfile(f)):
+                os.remove(f)
+            else:
+                os.rmdir(f)
+
+class TestEncryptedZip(unittest.TestCase):
+    filename = 'lhaplus-zkenc.zip'
+    encoding = 'sjis'
+    expected = [u'ローマ字テキスト.txt', u'秘密/パスワード.txt']
+    password = '全角暗号'
+
+    # ListZip should work without password
+    def testListZip(self):
+        result = unzipmbcs.listZip(self.filename, self.encoding)
+        self.assertEqual(list(map(lambda x: x[0], result)), self.expected)
+
+    def testExtractWithoutPassword(self):
+        with self.assertRaises(RuntimeError):
+            unzipmbcs.extractZip(self.filename, self.encoding)
+
+    def testExtractWithWrongPassword(self):
+        with self.assertRaises(RuntimeError):
+            unzipmbcs.extractZip(self.filename, self.encoding, password='wrongpass')
+
+    def testExtractWithPassword(self):
+        unzipmbcs.extractZip(self.filename, self.encoding, password=self.password)
         map(lambda x: self.assertTrue(os.path.exists(x), x + ' not exist'),
             self.expected)
 
